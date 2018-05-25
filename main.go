f5050f8dd227e8efcc00d1e5006113c98f9c8498
Grant Azure <you@example.com>
2018-05-25 13:17:36 -0700

    trivial client credentials implementation

--- main.go
+++ main.go
@@ -1,38 +1,72 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+)
 
-const POST_PARAM_CLIENT_ID = "client_id"
-const POST_PARAM_CLIENT_SECRET = "client_secret"
-const POST_PARAM_SCOPE = "scope"
-const POST_PARAM_GRANT_TYPE = "grant_type"
+const paramClientId = "client_id"
+const paramClientSecret = "client_secret"
+const postParamScope = "scope"
+const paramGrantType = "grant_type"
 
+const GrantTypeClientCredentials = "client_credentials"
 
 type user struct {
-	name string
+	name     string
 	password string // TODO: not this
 }
-
 
 var USERS = []user{
 	{name: "Grant", password: "987654321"},
 	{name: "Jim", password: "password"},
 }
 
+func validateClientCredentials(clientId string, clientSecret string) error {
+	err := errors.New("invalid user name or password")
+	for _, element := range USERS {
+		if element.name == clientId {
+			if element.password != clientSecret {
+				return err
+			}
+			return nil
+		}
+	}
+	return err
+}
 
-func bad_request(c *gin.Context) {
+func badRequest(c *gin.Context) {
 	c.JSON(400, gin.H{
 		"error": "no grant_type specified",
 	})
 }
 
-func token(c *gin.Context) {
-	grantType := c.PostForm("grant_type")
-	if grantType == "" {
-		bad_request(c)
+func handleClientCredentials(c *gin.Context) {
+	err := validateClientCredentials(c.PostForm(paramClientId), c.PostForm(paramClientSecret))
+	if err != nil {
+		c.JSON(401, gin.H{
+			"error": "not authorized",
+		})
 		return
 	}
+	c.JSON(200, gin.H{
+		"access_token":  "foo",
+		"refresh_token": "bar",
+		"scope":         c.PostForm(postParamScope),
+		"expires_in":    "never",
+	})
 
+}
+
+func token(c *gin.Context) {
+	grantType := c.PostForm(paramGrantType)
+	if grantType == "" {
+		badRequest(c)
+		return
+	}
+	if grantType == GrantTypeClientCredentials {
+		handleClientCredentials(c)
+	}
 }
 
 func health(c *gin.Context) {
